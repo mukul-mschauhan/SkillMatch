01ecb1b248041a8f97378dc40575bdf163b87652
Mukul Kumar Singh Chauhan <mukul.mschauhan@gmail.com>
2024-09-16 08:50:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,6 @@ if submit:
     
 
 
-st.divider()
 st.subheader("Disclaimer: ", divider = True)
 notes = f'''
 1. The ATS score and probability of success are estimates and should be used as guidance, not definitive predictions.
